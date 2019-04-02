4271fd2f01f84fb3022867be61cb7716de5563b6
loic.forest <loic.forest@hardis.fr>
2019-04-02 09:49:20 +0200

    Setting database code

---
+++ GamothequeWPF/Context.cs
@@ -0,0 +1,41 @@
+﻿using GamothequeWPF.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamothequeWPF
+{
+    class Context : DbContext
+    {
+        private static Context _context = null;
+        public async static Task<Context> GetCurrent()
+        {
+            if (_context == null)
+            {
+                _context = new Context(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "database.db"));
+                await _context.Database.MigrateAsync();
+            }
+
+            return _context;
+        } 
+
+        public Context(string databasePath) : base()
+        {
+            DatabasePath = databasePath;
+        }
+
+        public string DatabasePath { get; }
+        public DbSet<Game> Game { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.EnableSensitiveDataLogging();
+            optionsBuilder.UseSqlite($"Filename={Database}", x => x.SuppressForeignKeyEnforcement());
+        }
+    }
+}
