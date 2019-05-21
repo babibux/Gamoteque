c0dc5fee4340f9d3e8e4357278a0ee2904e81f5d
Barbara B <34717718+babibux@users.noreply.github.com>
2019-05-21 12:01:18 +0200

    fin du cours

--- GamothequeWPF/Views/MainWindow.xaml.cs
+++ GamothequeWPF/Views/MainWindow.xaml.cs
@@ -10,13 +10,12 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace GamothequeWPF
+namespace GamothequeWPF.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Logique d'interaction pour MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
@@ -24,7 +23,5 @@ namespace GamothequeWPF
         {
             InitializeComponent();
         }
-
-
     }
 }
