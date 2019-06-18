0749a229c2329526d4054f2861522c4d00d9245a
Barbara B <34717718+babibux@users.noreply.github.com>
2019-06-18 18:20:01 +0200

    WIP affichage partie gauche dynamique

---
+++ GamothequeWPF/ViewModel/GameDetailsViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamothequeWPF.ViewModel
+{
+    class GameDetailsViewModel:BaseNotifyPropertyChanged
+    {
+        public GameDetailsViewModel()
+        {
+            getAllGames();
+        }
+
+        public ObservableCollection<Model.Type> TypeList
+        {
+            get
+            {
+                return (ObservableCollection<Model.Type>)GetProperty();
+            }
+
+            set
+            {
+                SetProperty(value);
+            }
+        }
+
+        public async void getAllGames()
+        {
+            var context = await Context.GetCurrent();
+            TypeList = new ObservableCollection<Model.Type>(context.Type.ToList());
+        }
+    }
+}
